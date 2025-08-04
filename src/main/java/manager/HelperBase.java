7c608b98fbf9ceb79c9dc54ad4951bd19fe6998f
EkaterinaMamedov <temp44x@gmail.com>
2025-08-04 20:34:14 +0300

    added login success

--- src/main/java/manager/HelperBase.java
+++ src/main/java/manager/HelperBase.java
@@ -1,6 +1,10 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class HelperBase {
     WebDriver wd;
@@ -9,5 +13,22 @@ public class HelperBase {
         this.wd = wd;
     }
 
+    public void type(By locator, String text) {
+        WebElement we = wd.findElement(locator);
+        we.click();
+        we.clear();
+        if (text != null) {
+            we.sendKeys(text);
+        }
+    }
 
+    public void click(By locator) {
+        WebElement we = wd.findElement(locator);
+        we.click();
+    }
+
+    public boolean isElementPresent(By locator) {
+        List<WebElement> list = wd.findElements(locator);
+        return list.size() > 0;
+    }
 }
